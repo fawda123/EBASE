b3c9fc5988896efffece470478a67ab8a38f42d9
fawda123 <mbafs2012@gmail.com>
2022-03-24 14:55:37 -0400

    example data added

---
+++ R/exdat.R
@@ -0,0 +1,36 @@
+#' Sample data from Appalachicola NERRS
+#'
+#' Sample data from Appalachicola NERRS
+#'
+#' @format A \code{data.frame} object with 27645 rows and 6 columns
+#' \describe{
+#'   \item{DateTimeStamp}{date and time, Pacific/Jamaica time zone, 15 minute time step}
+#'   \item{DO_obs}{dissolved oxygen, mg/L}
+#'   \item{Temp}{water temperature, C}
+#'   \item{Sal}{salinity, psu}
+#'   \item{PAR}{total PAR, W/m2/s}
+#'   \item{WSpd}{num}
+#' }
+#' @family utilities
+#' 
+#' @details The DO time series has been detided using weighted regression in WtRegDO: \url{https://github.com/fawda123/WtRegDO}
+#' 
+#'\dontrun{
+#' library(dplyr)
+#'
+#' exdat <- read.csv('data-raw/APNERR2012dtd.csv') %>% 
+#'   rename(
+#'     PAR = I,
+#'     Sal = salinity,
+#'     Temp = tempC, 
+#'     DO_obs = DO.meas
+#'   ) %>% 
+#'   unite('DateTimeStamp', Date, Time, sep = ' ') %>% 
+#'   mutate(
+#'     DateTimeStamp = lubridate::ymd_hms(DateTimeStamp, tz = 'America/Jamaica')
+#'   ) %>% 
+#'   select(DateTimeStamp, DO_obs, Temp, Sal, PAR, WSpd) 
+#' 
+#' save(exdat, file = 'data/exdat.RData')
+#'}
+"exdat"
