1a0761f77cf06016df5198d2a5d2560680d0c2a9
fawda123 <mbafs2012@gmail.com>
2022-06-20 12:54:36 -0400

    adding arguments to fit_plot for scatter and bygroup #5

--- R/fit_plot.R
+++ R/fit_plot.R
@@ -1,11 +1,14 @@
 #' Plot observed and modeled dissolved oxygen
 #' 
 #' @param res output data frame from \code{\link{ebase}}
+#' @param bygroup logical indicating if the plot is faceted by group
+#' @param scatter logical indicating if a scatter plot of modeled versus estimated dissolved oxygen is returned
+#' @param showfit logical indicating if a linear fit is shown in the plot, applies only if \code{scatter = TRUE}
 #'
 #' @return A \code{\link[ggplot2]{ggplot}} object
 #' @export
 #'
-#' @details Dissolved oxygen (mmol/m3) is plotted as observed from the input data (points) and modelled (lines) based on inputs to \code{\link{ebase}} 
+#' @details Dissolved oxygen (mmol/m3) is plotted as observed from the input data (points) and modeled (lines) based on inputs to \code{\link{ebase}} if \code{scatter = FALSE}.  A scatter plot of modeled versus estimated dissolved oxygen is returned if \code{scatter = TRUE}, including a linear fit if \code{showfit = TRUE}.  The plot is faceted by group based on the \code{ndays} argument to \code{\link{ebase}} if \code{bygroup = TRUE}.
 #' 
 #' @examples 
 #' 
@@ -30,25 +33,59 @@
 #' 
 #' # plot observed and modeled DO
 #' fit_plot(res)
-fit_plot <- function(res){
+#' 
+#' # plot observed and modeled DO by group
+#' fit_plot(res, bygroup = TRUE)
+#' 
+#' # as scatter plot
+#' fit_plot(res, scatter = TRUE)
+#' 
+#' # as scatter plot by group
+#' fit_plot(res, scatter = TRUE, bygroup = TRUE)
+#' 
+fit_plot <- function(res, bygroup = FALSE, scatter = FALSE, showfit = TRUE){
     
-  toplo <- res
-  
-  p <- ggplot2::ggplot(toplo, ggplot2::aes(x = DateTimeStamp, y = DO_obs)) + 
-    ggplot2::geom_point(ggplot2::aes(color = 'Observed')) + 
-    ggplot2::geom_line(ggplot2::aes(y = DO_mod, color = 'Estimated')) + 
-    ggplot2::theme_minimal() + 
-    ggplot2::theme(
-      legend.position = 'top'
-    ) +
-    ggplot2::guides(
-      color = ggplot2::guide_legend(override.aes = list(shape = c(NA, 16), linetype = c(1, NA)))
-    ) +
-    ggplot2::labs(
-      y = "Dissolved Oxygen (mmol/m3)",
-      color = NULL,
-      x = NULL
+  toplo <- res %>% 
+    dplyr::mutate(
+      grp = paste('Group', grp)
     )
+  
+  if(!scatter)
+    p <- ggplot2::ggplot(toplo, ggplot2::aes(x = DateTimeStamp, y = DO_obs)) + 
+      ggplot2::geom_point(ggplot2::aes(color = 'Observed')) + 
+      ggplot2::geom_line(ggplot2::aes(y = DO_mod, color = 'Estimated')) + 
+      ggplot2::theme_minimal() + 
+      ggplot2::theme(
+        legend.position = 'top'
+      ) +
+      ggplot2::guides(
+        color = ggplot2::guide_legend(override.aes = list(shape = c(NA, 16), linetype = c(1, NA)))
+      ) +
+      ggplot2::labs(
+        y = "Dissolved Oxygen (mmol/m3)",
+        color = NULL,
+        x = NULL
+      )
+  
+  if(scatter){
+    
+    p <- ggplot2::ggplot(toplo, ggplot2::aes(x = DO_obs, y = DO_mod)) + 
+      ggplot2::geom_point() + 
+      ggplot2::theme_minimal() +
+      ggplot2::labs(
+        y = "Modeled Dissolved Oxygen (mmol/m3)",
+        x = "Observed Dissolved Oxygen (mmol/m3)"
+      )
+    
+    if(showfit)
+      p <- p +       
+        ggplot2::geom_smooth(formula = y ~ x, method = 'lm', se = FALSE)
+    
+  }
+  
+  if(bygroup)
+    p <- p + 
+      facet_wrap(~grp, scales = 'free')
   
   return(p)
   
--- tests/testthat/test-fit_plot.R
+++ tests/testthat/test-fit_plot.R
@@ -2,3 +2,13 @@ test_that("Check output for fit_plot", {
   result <- fit_plot(res)
   expect_s3_class(result, 'ggplot')
 })
+
+test_that("Check output for fit_plot, scatter = TRUE", {
+  result <- fit_plot(res, scatter = TRUE)
+  expect_s3_class(result, 'ggplot')
+})
+
+test_that("Check output for fit_plot, bygroup = TRUE", {
+  result <- fit_plot(res, bygroup = TRUE)
+  expect_s3_class(result, 'ggplot')
+})
