a51fc5acb1198f1c51f59f94212cc07d0b4a3fdc
fawda123 <mbafs2012@gmail.com>
2022-10-18 17:32:30 -0400

    fix timezone typo for exdat

--- R/exdat.R
+++ R/exdat.R
@@ -4,7 +4,7 @@
 #'
 #' @format A \code{data.frame} object with 27648 rows and 6 columns
 #' \describe{
-#'   \item{DateTimeStamp}{date and time, Pacific/Jamaica time zone, 15 minute time step}
+#'   \item{DateTimeStamp}{date and time, America/Jamaica time zone, 15 minute time step}
 #'   \item{DO_obs}{dissolved oxygen, mg/L}
 #'   \item{Temp}{water temperature, C}
 #'   \item{Sal}{salinity, psu}
